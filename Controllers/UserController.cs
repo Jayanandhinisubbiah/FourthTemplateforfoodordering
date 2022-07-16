0ceb76dffb023f7d03fc690c2402a77e028edad9
Jayanandhini S <jayanandhini@whiteblue.in>
2022-07-16 17:12:05 +0530

    nnn

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using FourthTemplateforfoodordering.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
 namespace FourthTemplateforfoodordering.Controllers
@@ -16,6 +18,23 @@ namespace FourthTemplateforfoodordering.Controllers
         {
             return View(fd.Products.ToList());
         }
+        //public IActionResult MyCart(OrderDetails O)
+        //{
+        //    return View(O);
+        //}
+        public IActionResult Buy(int? FoodId)
+        {
+            var result = fd.Products.Find(FoodId);
+            HttpContext.Session.SetString("Price", result.Price.ToString());
+            return RedirectToAction("Payment");
+        }
+        public IActionResult Payment()
+        {
+            ViewBag.Price = HttpContext.Session.GetString("Price");
 
+
+            return View();
+
+        }
     }
 }
