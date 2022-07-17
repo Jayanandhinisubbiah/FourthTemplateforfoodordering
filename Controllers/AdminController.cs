54c5ad68ed20ad920b6562ab5118f49d51955af6
Jayanandhini S <jayanandhini@whiteblue.in>
2022-07-17 13:11:46 +0530

    jjj

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -13,42 +13,47 @@ namespace FourthTemplateforfoodordering.Controllers
         {
             this.fd = fd;
         }
-        public IActionResult Index()
-        {
-            return View(fd.Products.ToList());
-        }
-        public IActionResult AddProduct()
-        {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult AddProduct(Product P)
-        {
-            // string uniqueFileName = ProcessUploadedFile(P);
-            fd.Products.Add(P);
-            fd.SaveChanges();
-            return RedirectToAction("Index");
-        }
-        public IActionResult ViewReport()
-        {
-            //var FoodContext = fd.OrderDetail.Include(n => n.OrderID.UserID.Email);
-           // return View(await applicationDbContext.ToListAsync());
-            return View(fd.OrderDetail.ToList());
-        }
+        //public IActionResult Index()
+        //{
+        //    return View(fd.Products.ToList());
+        //}
+        //public IActionResult AddProduct()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public IActionResult AddProduct(Product P)
+        //{
+        //    // string uniqueFileName = ProcessUploadedFile(P);
+        //    fd.Products.Add(P);
+        //    fd.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+        //public IActionResult ViewReport()
+        //{
+        //    //var FoodContext = fd.OrderDetail.Include(n => n.OrderID.UserID.Email);
+        //   // return View(await applicationDbContext.ToListAsync());
+        //    return View(fd.OrderDetail.ToList());
+        //}
 
 
 
 
-        public IActionResult NewOrder()
+        //public IActionResult NewOrder()
+        //{
+        //    return View(fd.OrderDetail.ToList());
+        //}
+        //public IActionResult Dispatch(int id)
+        //{
+        //    Product P = fd.Products.Find(id);
+        //    fd.Products.Remove(P);
+        //    fd.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+        public IActionResult Orders()
         {
-            return View(fd.OrderDetail.ToList());
-        }
-        public IActionResult Dispatch(int id)
-        {
-            Product P = fd.Products.Find(id);
-            fd.Products.Remove(P);
-            fd.SaveChanges();
-            return RedirectToAction("Index");
+            return View(fd.OrderMasters.ToList());
+
         }
     }
 }
